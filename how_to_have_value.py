a0157dc301cea65939f0926b34579be111a6b489
Thiago Silva <53449883+tsdev96@users.noreply.github.com>
2024-01-11 17:30:32 -0300

    Create how_to_have_value
    Coisas que eu aprendi. Não desista.

---
+++ how_to_have_value.py
@@ -0,0 +1,18 @@
+def to_contribute(self_investment, goal):
+    to_grow = self_investment  # Specialize, learn more, be curious.
+
+    purpose = goal # People may objectify you. Don't be just a tool.
+
+    if to_grow == True and purpose == True:
+        return "I am valuable"
+    else:
+        return "Start over, there may be just one line wrong." 
+                                # Don't stop writing.
+
+self_investment = True
+
+goal = True
+
+print(to_contribute(self_investment, goal))
+
+# Don't stop writing.
